541142772d03dc2777f85cd082de9f69c31defd7
JANAKIraman2@ <digitalexsolutions@gmail.com>
2025-06-23 12:58:21 +0530

    shimmer loading widget

--- lib/app/views/homescreen.dart
+++ lib/app/views/homescreen.dart
@@ -9,6 +9,7 @@ import 'package:expense_tracker/helper/datehelper.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -34,92 +35,100 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Consumer<Homeprovider>(builder: (context, provider, _) {
-      return provider.isloading
-          ? const Center(child: CircularProgressIndicator())
-          : Scaffold(
-              backgroundColor:
-                  provider.isdarkmode ? Appcolors.darkcolor : Colors.white,
-              appBar: AppBar(
-                backgroundColor: provider.isdarkmode
-                    ? const Color.fromARGB(221, 0, 0, 43)
-                    : Colors.white,
-                automaticallyImplyLeading: false,
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Profile()));
-                          },
-                          child: CircleAvatar(
-                            radius: 20,
-                            backgroundImage: provider.localimage!.length > 1
-                                ? FileImage(File(provider.localimage!))
-                                : provider.profileimage!.length > 1
-                                    ? NetworkImage(
-                                        Apis.baseurl + provider.profileimage!)
-                                    : AssetImage(Appimage.profileicon),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 6,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Welcome",
-                              style: TextStyle(
-                                color: provider.isdarkmode
-                                    ? Colors.white
-                                    : Colors.black,
-                                fontSize: 12,
-                              ),
-                            ),
-                            Text(
-                              provider.name.text,
-                              style: TextStyle(
-                                color: provider.isdarkmode
-                                    ? Colors.white
-                                    : Colors.black,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
+      return Scaffold(
+        backgroundColor:
+            provider.isdarkmode ? Appcolors.darkcolor : Colors.white,
+        appBar: AppBar(
+          backgroundColor: provider.isdarkmode
+              ? const Color.fromARGB(221, 0, 0, 43)
+              : Colors.white,
+          automaticallyImplyLeading: false,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Profile()));
+                    },
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: provider.localimage!.length > 1
+                          ? FileImage(File(provider.localimage!))
+                          : provider.profileimage!.length > 1
+                              ? NetworkImage(
+                                  Apis.baseurl + provider.profileimage!)
+                              : AssetImage(Appimage.profileicon),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        context.go('/home/settings');
-                      },
-                      child: const Icon(
-                        Icons.settings,
-                        color: Colors.grey,
-                        size: 33,
+                  ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome",
+                        style: TextStyle(
+                          color:
+                              provider.isdarkmode ? Colors.white : Colors.black,
+                          fontSize: 12,
+                        ),
                       ),
-                    ),
-                  ],
+                      Text(
+                        provider.name.text,
+                        style: TextStyle(
+                          color:
+                              provider.isdarkmode ? Colors.white : Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              GestureDetector(
+                onTap: () {
+                  context.go('/home/settings');
+                },
+                child: const Icon(
+                  Icons.settings,
+                  color: Colors.grey,
+                  size: 33,
                 ),
               ),
-              body: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 26.0),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        context.go('/home/manage');
-                      },
-                      child: Container(
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 26.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: () {
+                  context.go('/home/manage');
+                },
+                child: provider.isloading
+                    ? Shimmer.fromColors(
+                        child: Container(
+                          height: 170,
+                          width: 320,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        baseColor: Colors.grey[300]!,
+                        highlightColor: Colors.white)
+                    : Container(
                         padding: const EdgeInsets.symmetric(
                             horizontal: 20, vertical: 20),
                         decoration: BoxDecoration(
@@ -255,40 +264,51 @@ class _HomescreenState extends State<Homescreen> {
                           ],
                         ),
                       ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Transactions",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color:
+                          provider.isdarkmode ? Colors.white : Colors.black87,
                     ),
-                    const SizedBox(
-                      height: 20,
+                  ),
+                  Text(
+                    "View all",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                      color:
+                          provider.isdarkmode ? Appcolors.white : Colors.black,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Transactions",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 20,
-                            color: provider.isdarkmode
-                                ? Colors.white
-                                : Colors.black87,
-                          ),
-                        ),
-                        Text(
-                          "View all",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 15,
-                            color: provider.isdarkmode
-                                ? Appcolors.white
-                                : Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                          itemCount: provider.debittransactions.length,
-                          itemBuilder: (context, index) {
-                            return Container(
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: provider.debittransactions.length,
+                    itemBuilder: (context, index) {
+                      return provider.isloading
+                          ? Shimmer.fromColors(
+                              child: Container(
+                                margin: EdgeInsets.symmetric(vertical: 10),
+                                height: 70,
+                                width: 320,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              baseColor: Colors.grey[300]!,
+                              highlightColor: Colors.white)
+                          : Container(
                               width: double.infinity,
                               margin: const EdgeInsets.symmetric(vertical: 10),
                               padding: const EdgeInsets.symmetric(
@@ -360,12 +380,12 @@ class _HomescreenState extends State<Homescreen> {
                                 ],
                               ),
                             );
-                          }),
-                    ),
-                  ],
-                ),
+                    }),
               ),
-            );
+            ],
+          ),
+        ),
+      );
     });
   }
 }
